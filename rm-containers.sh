7308b1adf27ab41b0620aefbf12a2beb94266fbe
SolidSnake <solid@snake>
2022-06-14 13:07:49 -0400

    clean up

--- rm-containers.sh
+++ rm-containers.sh
@@ -1,7 +1,10 @@
-if [ -z $(docker ps -q -a)]
+if [ -z $(docker ps -q -a --filter 'status=exited') ]
 then
     echo "Nothing to delete"
 else
-    echo "Deleting containers..."
-    docker rm $(docker ps -q -a)
+    echo "Listing containers with exited status"
+    docker ps -a --filter 'status=exited'
+    echo "\n"
+    echo "Deleting containers with exited status"
+    docker rm $(docker ps -q -a --filter 'status=exited')
 fi
