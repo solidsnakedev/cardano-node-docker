f4acaa903dc870c1141dcc6f087acd00ac7cf58b
SolidSnake <solid@snake>
2022-06-14 16:23:50 -0400

    adding more commands

--- docker-cardano-cli-stats.sh
+++ docker-cardano-cli-stats.sh
@@ -1,1 +1,8 @@
-docker run --rm -it -v cardano-node-db:/root/node/db cardano-node /root/.local/bin/cardano-cli query tip --testnet-magic 1097911063
+container=$(docker ps -q --filter ancestor=cardano-node)
+
+echo -e "\n### Printing cardano-cli version ###\n"
+docker exec $container /root/.local/bin/cardano-cli --version
+echo -e "\n### Printing cardano-node logs ###\n"
+docker exec $container tail /root/node/logs/node.log
+echo -e "\n### Printing tip of the blockchain ###\n"
+docker exec $container /root/.local/bin/cardano-cli query tip --testnet-magic 1097911063
