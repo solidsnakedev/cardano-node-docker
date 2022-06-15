fda99734dbb8ff7fcda50f07af43e30b2888cd2f
SolidSnake <solid@snake>
2022-06-15 08:11:36 -0400

    add info

--- docker-backup-db.sh
+++ docker-backup-db.sh
@@ -1,3 +1,6 @@
 #scp -r /root/node/db workstation1@192.168.2.27:/home/workstation1/Documents/projects/cardano-node-docker
 container=$(docker ps -q --filter ancestor=cardano-node)
+echo -e "\nBacking up cardano-node database from container $container:/root/node/db to host db directory"
 docker cp $container:/root/node/db db
+echo -e "\nCardano node db succesfully copied!! \nTotal size directory:"
+du -h -c db
