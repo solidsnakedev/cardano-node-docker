d7c5271c08509cfe0f3e3ef2d9005a473899dd0c
SolidSnake <solid@snake>
2022-06-15 07:57:22 -0400

    delete script

--- docker-backup-db.sh
+++ docker-backup-db.sh
@@ -1,1 +1,3 @@
-scp -r /root/node/db workstation1@192.168.2.27:/home/workstation1/Documents/projects/cardano-node-docker
+#scp -r /root/node/db workstation1@192.168.2.27:/home/workstation1/Documents/projects/cardano-node-docker
+container=$(docker ps -q --filter ancestor=cardano-node)
+docker cp $container:/root/node/db db
