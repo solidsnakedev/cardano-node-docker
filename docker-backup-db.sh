36587ab64480bb98e654ae93101c0f5a7df8bc61
SolidSnake <solid@snake>
2022-06-18 13:10:13 -0400

    update script

--- docker-backup-db.sh
+++ docker-backup-db.sh
@@ -1,6 +1,23 @@
-#scp -r /root/node/db workstation1@192.168.2.27:/home/workstation1/Documents/projects/cardano-node-docker
-container=$(docker ps -q --filter ancestor=cardano-node)
-echo -e "\nBacking up cardano-node database \nFrom container $container:/root/node/db \nTo host $(pwd)/db"
-docker cp $container:/root/node/db db
-echo -e "\nCardano node db succesfully copied!! \nTotal size directory:"
-du -h -c db
+backup_db(){
+    container=$(docker ps -q --filter ancestor=cardano-node)
+    echo -e "\n- Backing up cardano-node database \nFrom container $container:/root/node/db \nTo host $(pwd)/db"
+    docker cp $container:/root/node/db .
+    echo -e "\n- Cardano node db succesfully copied!! \nTotal size directory:"
+    du -h -c db
+}
+
+if [[ -d db ]]; then
+    echo -e "\n- Found db directory in $(pwd)"
+    ls db
+    echo -e "\n- Delete db folder, then run backup? (y/n)"
+    read ans
+    if [[ ${ans} == "y" ]]; then
+        rm -r -v db
+        backup_db
+    else
+        echo -e "- Nothing to backup"
+    fi
+else
+    backup_db
+fi
+
