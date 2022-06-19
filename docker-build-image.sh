42dd2984b5358f78a23bb1351c94198d1d69e4da
SolidSnake <solid@snake>
2022-06-19 10:42:04 -0400

    update scripts

--- docker-build-image.sh
+++ docker-build-image.sh
@@ -1,3 +1,19 @@
-docker build -t cardano-node .
-#docker build -f Dockerfile.build -t cardano-node-test .
-#docker build -f Dockerfile.dev -t cardano-node-test .
+echo -e "\n- Docker files found :"
+ls  Dockerfile*
+
+echo -e "\n- Enter Dockerfile name : "
+read dockerfile
+
+if [[ -e ${dockerfile} ]]
+then
+    if [[ ${dockerfile} == "Dockerfile.build" ]]
+    then
+        echo -e "creating cardano-node-build\n"
+        docker build -f ${dockerfile} -t cardano-node-build .
+    else
+        echo -e "creating cardano-node-dev\n"
+        docker build -f ${dockerfile} -t cardano-node-dev .
+    fi
+else
+    echo "Dockerfile does not exists!"
+fi
