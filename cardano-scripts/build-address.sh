5c26a5d0bc168351511a6359e67be1e555142e27
SolidSnake <solid@snake>
2022-06-28 10:38:55 -0400

    update

--- cardano-scripts/build-address.sh
+++ cardano-scripts/build-address.sh
@@ -7,10 +7,10 @@ if [[ -e /node/keys/${key}.vkey && -e /node/keys/${stake}.vkey ]]
 then
     echo -e "\nCreating/Deriving cardano address from verification key"
     cardano-cli address build \
-    --payment-verification-key-file /node/keys/${key}.vkey \
-    --stake-verification-key-file /node/keys/${stake}.vkey \
-    --out-file /node/keys/${key}.addr \
-    --testnet-magic $TESNET_MAGIC
+        --payment-verification-key-file /node/keys/${key}.vkey \
+        --stake-verification-key-file /node/keys/${stake}.vkey \
+        --out-file /node/keys/${key}.addr \
+        --testnet-magic $TESNET_MAGIC
     echo -e "\nCardano address created"
     ls /node/keys/$key.addr
     echo -e "\n"
