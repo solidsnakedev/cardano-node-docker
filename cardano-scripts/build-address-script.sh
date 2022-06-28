09bd9cd71727edf7d9b3d9aac403cd4138de2c8d
SolidSnake <solid@snake>
2022-06-28 10:59:52 -0400

    adding build address script

---
+++ cardano-scripts/build-address-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo -e "\nScripts found" && ls /node/scripts/
+read -p "Insert plutus script name (example AlwaysSucced): " script
+
+if [[ -e /node/scripts/${script}.plutus ]]
+then
+    echo -e "\nCreating/Deriving cardano address from plutus script"
+    cardano-cli address build \
+        --payment-script-file /node/scripts/${script}.plutus \
+        --testnet-magic ${TESTNET_MAGIC} \
+        --out-file /node/keys/${script}.addr
+    echo -e "\nPlutus script address created $(ls /node/scripts/${script}.addr) \n"
+else
+    echo -e "Plutus script does not exists! \nPlease build a plutus script\n"
+fi
