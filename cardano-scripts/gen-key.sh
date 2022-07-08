fc5eae30ca180a9ce5309913b6fe8a4045944a8f
SolidSnake <solid@snake>
2022-07-08 12:06:43 -0400

    improve parameter inputs

--- cardano-scripts/gen-key.sh
+++ cardano-scripts/gen-key.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-set -euo pipefail
+set -o pipefail
 
 #--------- Import common paths and functions ---------
 source common.sh
 
 #--------- Run program ---------
-read -p "Insert key name (example payment1) : " payment
-read -p "Insert stake key name (example stake1) : " stake
+
+if [[ "$#" -eq 0 ]]; then echo_red "Error: Missing parameters" && echo_yellow "Info: Command example -> gen-key.sh payment1 stake1 | gen-key.sh payment1"; exit 1; fi
+payment=${1}
 
 ${cardanocli} address key-gen \
     --verification-key-file ${key_path}/${payment}.vkey \
     --signing-key-file ${key_path}/${payment}.skey
 
+echo_green "- Keys saved in ${key_path}/${payment}.vkey and ${key_path}/${payment}.skey "
+
+if [[ -z ${2} ]]; then exit 0; fi
+stake=${2}
+
 ${cardanocli} stake-address key-gen \
     --verification-key-file ${key_path}/${stake}.vkey \
     --signing-key-file ${key_path}/${stake}.skey
 
-echo_green "\nKeys saved in ${key_path}"
+echo_green "- Keys saved in ${key_path}/${stake}.vkey and ${key_path}/${stake}.skey"
--- cardano-scripts/query-utxo.sh
+++ cardano-scripts/query-utxo.sh
@@ -4,22 +4,13 @@
 source common.sh
 
 #--------- Run program ---------
-if [[ -z $1 ]]
-then
-    echo_green "\n- List of addresses :" && ls -1 ${key_path}/*.addr
-    read -p "Insert wallet address (example payment1): " wallet
-else
-    wallet=$1
-fi
+if [[ "$#" -eq 0 || "$#" -ne 1 ]]; then echo_red "Error: Missing parameters" && echo_yellow "Info: Example -> query-utxo payment1"; exit 1; fi
 
-if [[ -e ${key_path}/${wallet}.addr ]]
-then
-    echo_green "- Address string value : $(cat ${key_path}/${wallet}.addr) "
-    echo_green "- Queryng adddress in cardano testnet ...\n"
-    ${cardanocli} query utxo \
-      --testnet-magic $TESTNET_MAGIC \
-      --address $(cat ${key_path}/${wallet}.addr)
-else
-    echo_red  "- Address does not exists!\n"
-fi
+wallet=$1
 
+if [[ ! -e ${key_path}/${wallet}.addr ]]; then echo_red  "- Address does not exists!"; exit 1; fi
+
+#echo_green "- Queryng adddress: $(cat ${key_path}/${wallet}.addr)"
+${cardanocli} query utxo \
+  --testnet-magic $TESTNET_MAGIC \
+  --address $(cat ${key_path}/${wallet}.addr)
